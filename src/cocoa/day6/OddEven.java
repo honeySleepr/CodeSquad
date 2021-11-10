87697a59beb7a88208c900ba2eedfff1baf2ed8c
honeySleepr <gruzzimo@naver.com>
2021-11-11 00:56:25 +0900

    Game 변수가 계속 리셋 됨..
    뭐가 문제일까

--- src/cocoa/day6/OddEven.java
+++ src/cocoa/day6/OddEven.java
@@ -4,9 +4,6 @@ public class OddEven {
     public static void main(String[] args) {
         Game game = new Game();
         game.init();
-        playerinfo.RegisterPlayer();
-        Game game = new Game();
-        game.RegisterPlayer();
         while (game.myMoney != 0 && game.numStage < 9) {
             game.repeat();
         }
