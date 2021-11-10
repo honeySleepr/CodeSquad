92a4f2f530fb210b1146fd133db4899447df126f
honeySleepr <gruzzimo@naver.com>
2021-11-11 01:59:33 +0900

    David 코드 리뷰 반영
    이제 돌아가게 해보자!!

--- src/cocoa/day6/OddEven.java
+++ src/cocoa/day6/OddEven.java
@@ -3,7 +3,6 @@ package cocoa.day6;
 public class OddEven {
     public static void main(String[] args) {
         Game game = new Game();
-        game.init();
         while (game.myMoney != 0 && game.numStage < 9) {
             game.repeat();
         }
