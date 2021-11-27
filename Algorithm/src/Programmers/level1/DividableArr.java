6a6fda04f5f9a8d5158a2caffb3838522f6ebafc
honeySleepr <gruzzimo@naver.com>
2021-11-28 00:02:43 +0900

    한문제라도!

---
+++ Algorithm/src/Programmers/level1/DividableArr.java
@@ -0,0 +1,36 @@
+package Programmers.level1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*나누어 떨어지는 숫자 배열*/
+public class DividableArr {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.solution(new int[]{2, 36, 1, 3}, 1);
+    }
+}
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] answer;
+        for (int i : arr) {
+            if (i % divisor == 0) {
+                list.add(i);
+            }
+        }
+        Collections.sort(list);
+        if (list.size() == 0) {
+            answer = new int[]{-1};
+        } else {
+            answer = new int[list.size()];
+        }
+
+        for (int j = 0; j < list.size(); j++) {
+            answer[j] = list.get(j);
+        }
+
+        return answer;
+    }
+}
