9402e29b55915457bcd2a84be0af8b8a81c28b0e
honeysleepr <gruzzimo@naver.com>
2021-12-12 02:09:33 +0900

    데일리 알고리즘
    +BJ1157 완성
    생각보다 훨씬 많이 헤맸다. 다른 사람들은 어떻게 풀었는지를 참고하여
    공부해보자.

--- Algorithm/src/BaekJoon/BJ1157.java
+++ Algorithm/src/BaekJoon/BJ1157.java
@@ -1,11 +1,9 @@
 package BaekJoon;
-
+//입력된 단어에서 가장 많이 사용된 알파벳을 출력. 단, 여기에 해당되는 알파벳이 두 개 이상인 경우 "?" 출력
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Locale;
 
 public class BJ1157 {
     public static void main(String[] args) {
@@ -17,11 +15,36 @@ public class BJ1157 {
             for (Character c : input.toCharArray()) {
                 letter.add(c);
             }
-            System.out.println(letter);
             br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(input);
+
+        int startIndex = 0;
+        int count = 0;
+        int maxCount = 0;
+        boolean onlyOne = true;
+        Character finalChar = '0';
+        for (Character c : letter) {
+            while (input.indexOf(c, startIndex) != -1) {
+                count++;
+                startIndex = input.indexOf(c, startIndex) + 1;
+            }
+            if (maxCount < count) {
+                onlyOne = true;
+                maxCount = count;
+                finalChar = c;
+
+            } else if (maxCount == count) {
+                onlyOne = false;
+            }
+            count = 0;
+            startIndex = 0;
+        }
+        if (!onlyOne) {
+            System.out.println("?");
+        }else{
+            System.out.println(finalChar);
+        }
     }
 }
